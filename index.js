cb8cff6d49b368f80a652775cae1021d332f9541
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-20 10:15:50 +0700

    Menambahkan Method DELETE

--- index.js
+++ index.js
@@ -81,3 +81,15 @@ app.put('/api/users/:id', (req,res) => {
         }
     );
 });
+
+//DELETE
+app.delete('/api/users/:id', (req,res) => {
+    const userId = req.params.id;
+    db.query('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            console.error (err);
+            return res.status(500).json({ message: 'Database Error'});
+        }
+        res.json({message: 'User deleted sussessfully!'});
+    });
+});
